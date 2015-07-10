1ad3955ecfab6167f592b98aacb242a260a66978
Alloffices <alloffices.nyc@gmail.com>
2015-07-10 11:49:11 -0400

    added edit and show views and made them work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
 	
 	def index
 		@posts =Post.all
@@ -10,9 +11,38 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update(params[:post].permit(:title, :content))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to root_path
 	end
 
 	private
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
 
 	def post_params
 		params.require(:post).permit(:title, :content)
